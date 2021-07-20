b5a8059634b8599cb7ea549bfec48ecbde74bc69
xian <xnslx89@gmail.com>
2021-07-19 21:18:48 -0500

    refactor

--- web/pages/index.js
+++ web/pages/index.js
@@ -16,63 +16,63 @@ function Home() {
 }
 
 // web/pages/index.js
-console.log(
-  "process.env.NEXT_PUBLIC_SHOPIFY_URL",
-  process.env.NEXT_PUBLIC_SHOPIFY_URL
-);
-console.log("process.env.NEXT_PUBLIC_TOKEN", process.env.NEXT_PUBLIC_TOKEN);
-export async function getStaticProps() {
-  const graphQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_SHOPIFY_URL, {
-    headers: {
-      "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_TOKEN,
-    },
-  });
-  const query = gql`
-    {
-      collectionByHandle(handle: "albums") {
-        id
-        title
-        products(first: 12) {
-          edges {
-            node {
-              id
-              title
-              variants(first: 1) {
-                edges {
-                  node {
-                    id
-                  }
-                }
-              }
-              images(first: 1) {
-                edges {
-                  node {
-                    altText
-                    transformedSrc
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
+// console.log(
+//   "process.env.NEXT_PUBLIC_SHOPIFY_URL",
+//   process.env.NEXT_PUBLIC_SHOPIFY_URL
+// );
+// console.log("process.env.NEXT_PUBLIC_TOKEN", process.env.NEXT_PUBLIC_TOKEN);
+// export async function getStaticProps() {
+//   const graphQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_SHOPIFY_URL, {
+//     headers: {
+//       "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_TOKEN,
+//     },
+//   });
+//   const query = gql`
+//     {
+//       collectionByHandle(handle: "albums") {
+//         id
+//         title
+//         products(first: 12) {
+//           edges {
+//             node {
+//               id
+//               title
+//               variants(first: 1) {
+//                 edges {
+//                   node {
+//                     id
+//                   }
+//                 }
+//               }
+//               images(first: 1) {
+//                 edges {
+//                   node {
+//                     altText
+//                     transformedSrc
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `;
 
-  const res = await graphQLClient.request(query);
+//   const res = await graphQLClient.request(query);
 
-  if (res.errors) {
-    console.log(JSON.stringify(res.errors, null, 2));
-    throw Error("Unable to retrieve Shopify Products. Please check logs");
-  }
+//   if (res.errors) {
+//     console.log(JSON.stringify(res.errors, null, 2));
+//     throw Error("Unable to retrieve Shopify Products. Please check logs");
+//   }
 
-  return {
-    props: {
-      data: {
-        collection: res.collectionByHandle,
-      },
-    },
-  };
-}
+//   return {
+//     props: {
+//       data: {
+//         collection: res.collectionByHandle,
+//       },
+//     },
+//   };
+// }
 
 export default Home;
