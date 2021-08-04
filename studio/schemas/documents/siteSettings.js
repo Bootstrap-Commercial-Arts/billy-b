e53937a6e5cae3b879167e53422045b2016cc2a8
Fannie Gunton <fanniegunton@gmail.com>
2021-08-04 17:41:45 -0500

    Make siteSettings a singleton

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -1,28 +1,29 @@
 export default {
-  name: 'siteSettings',
-  title: 'siteSettings',
-  type: 'document',
+  name: "siteSettings",
+  title: "siteSettings",
+  type: "document",
   fields: [
     {
-      title: 'Site Name',
-      name: 'siteName',
-      type: 'string',
-      validation: Rule => Rule.required()
+      title: "Site Name",
+      name: "siteName",
+      type: "string",
+      validation: (Rule) => Rule.required(),
     },
     {
-        title: 'Email',
-        name: 'email',
-        type: 'string'
+      title: "Email",
+      name: "email",
+      type: "string",
     },
     {
-        title:'Phone',
-        name: 'phone',
-        type: 'string'
+      title: "Phone",
+      name: "phone",
+      type: "string",
     },
     {
-      title: 'SEO Settings',
-      name: 'seoSettings',
-      type: 'seoSettings'
-    }
+      title: "SEO Settings",
+      name: "seoSettings",
+      type: "seoSettings",
+    },
   ],
-}
+  __experimental_actions: [/*'create',*/ "update", /*'delete',*/ "publish"],
+};
