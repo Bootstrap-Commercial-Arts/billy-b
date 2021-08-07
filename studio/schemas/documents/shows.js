9acc4d89d2b7bcea5996cb55d4fcb076af129864
Gamel James <g_james@apple.com>
2021-08-06 17:26:11 -0700

    Add Refernce to Songs in Shows

--- studio/schemas/documents/shows.js
+++ studio/schemas/documents/shows.js
@@ -1,21 +1,33 @@
 import { FiSpeaker } from "react-icons/fi";
 
 export default {
-  name: "shows",
-  title: "Shows",
-  type: "document",
-  icon: FiSpeaker,
-  fields: [
-    {
-      name: "title",
-      title: "Title",
-      type: "string",
-    },
-    {
-      name: "guide",
-      title: "Teacher Guide",
-      type: "reference",
-      to: [{ type: "teacherGuides" }],
-    },
-  ],
+    name: "shows",
+    title: "Shows",
+    type: "document",
+    icon: FiSpeaker,
+    fields: [{
+            name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "guide",
+            title: "Teacher Guide",
+            type: "reference",
+            to: [{ type: "teacherGuides" }],
+        },
+        {
+            name: "songs",
+            title: "Songs",
+            type: "array",
+            of: [{
+                title: "Songs",
+                type: "reference",
+                to: [{
+                    title: "Songs",
+                    type: "lyrics",
+                }, ],
+            }, ],
+        },
+    ],
 };
