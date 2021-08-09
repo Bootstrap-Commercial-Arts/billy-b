77076a2a49a5945b9e1d81c01e7f59769367ce5e
bythebootstrap <schad@bythebootstrap.us>
2021-08-09 09:33:33 -0700

    maxWidth for pages

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -32,7 +32,7 @@ const Layout = props => {
         <div
           css={{
             margin: "0 auto",
-            [breakpoints.extraLarge]: { width: "1200px", margin: "0 auto" },
+            maxWidth: "1200px",
           }}
         >
           <Header />
