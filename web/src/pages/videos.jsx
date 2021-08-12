76d16039194c0010f32029f7c3c73b4143f5ded7
bythebootstrap <schad@bythebootstrap.us>
2021-08-12 07:43:48 -0700

    removed link to nathan script

--- web/src/pages/videos.jsx
+++ web/src/pages/videos.jsx
@@ -7,16 +7,6 @@ import Layout from "../components/Layout"
 import videosHero from "../assets/videos-hero.jpg"
 import theme from "../styles/theme"
 
-
-componentDidMount () {
-  const script = document.createElement("script");
-
-  script.src = "https://use.typekit.net/foobar.js";
-  script.async = true;
-
-  document.body.appendChild(script);
-}
-
 const VideosPage = () => (
   
   
