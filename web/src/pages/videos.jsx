aa933c6beb9f5fdb227ada8c2dbb15508febbabc
bythebootstrap <schad@bythebootstrap.us>
2021-08-12 07:29:19 -0700

    debug videos

--- web/src/pages/videos.jsx
+++ web/src/pages/videos.jsx
@@ -15,28 +15,14 @@ class Application extends React.Component {
         <div className="application">
             <Helmet>
                 <meta charSet="utf-8" />
-                <script src="../assets/video-page/YT-script.js" type="text/javascript" />
+                <script src="" type="text/javascript" />
             </Helmet>
-            ...
         </div>
     );
   }
 };
 
 const VideosPage = () => (
-  //const [YTVideos, setYTVideos] = useState(testData)
-  // useEffect(() => {
-
-  // }, [YTVideos])
-/*
-  const loadData = async () => {
-    const response = await fetch(
-      "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLFszJqZtLxVnHCxHlnTHU468qPA1hHNRa&key=AIzaSyCbaV62f8OKSaU8hURXHixccwL4Vg7-Fe8"
-    )
-    const data = await response.json()
-    setYTVideos(data)
-  }
-  loadData()*/
   
   
 
@@ -78,79 +64,3 @@ const VideosPage = () => (
 
 
 export default VideosPage
-/*
-// Check if window is defined (so if in the browser or in node.js).
-const isBrowser = typeof window !== "undefined"
-export default function MyComponent() {
-  let loggedIn = false
-  if (isBrowser) {
-    window.localstorage.getItem("isLoggedIn") === "true"
-  }
-      window.onload = () => {
-        let apiUrl =
-          "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLFszJqZtLxVnHCxHlnTHU468qPA1hHNRa&key=AIzaSyCbaV62f8OKSaU8hURXHixccwL4Vg7-Fe8";
-
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data.items);
-            data.items.forEach((item) => {
-              createVideoElement(item.snippet.resourceId.videoId);
-            });
-          })
-          .catch((err) => {
-            alert(err);
-          });
-      };
-
-      function createVideoElement(videoId) {
-        let appendTarget = document.getElementById("target");
-        let container = document.createElement("div");
-        let frame = document.createElement("iframe");
-
-        container.className = "video";
-        frame.src = `https://www.youtube.com/embed/${videoId}`;
-        frame.width = 560;
-        frame.height = 315;
-        frame.frameborder = 0;
-        frame.allowFullscreen = true;
-
-        container.append(frame);
-        target.append(container);
-      }
-};
-
-/* COPYPASTE FROM HERE TO NEXT COMMENT TO ADD A NEW VIDEO }
-        {YTVideos.items.map(video => {
-          return (
-            <div
-              css={{
-                padding: "0.5rem",
-                marginBottom: "0.5rem",
-                [theme.mobile]: {
-                  padding: 0,
-                },
-              }}
-              key={video.snippet.resourceId.videoId}
-            >
-              <iframe
-                src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`}
-                css={{
-                  width: "100%",
-                  height: "16rem",
-                  border: "none",
-                }}
-              ></iframe>
-              <h3
-                css={{
-                  paddingTop: "0.5rem",
-                  fontSize: 14,
-                  fontStyle: "italic",
-                }}
-              >
-                {video.snippet.title}
-              </h3>
-            </div>
-          )
-        })}
-        { TO HERE */
